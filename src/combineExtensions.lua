ad2848620c89c65578efa3ccad7b7b90bf92eb96
Matteo Autieri <matteoautieri@live.it>
2017-03-08 15:36:33 +0100

    almost done

--- src/combineExtensions.lua
+++ src/combineExtensions.lua
@@ -13,9 +13,7 @@ function CombineExtensions:update(dt)
         if fillLevel >= (0.75 * capacity) and not self.beaconLightsFL75 then
             self.beaconLightsFL75 = true;
             self:setBeaconLightsVisibility(true, false);
-            if self.isEntered then
-                playSample(self.signalSample, 1, 1.2, 0);
-            end
+            CombineExtensions.playSample(self);
         elseif fillLevel <= (0.75 * capacity) and self.beaconLightsFL75 then
             self:setBeaconLightsVisibility(false, false);
             self.beaconLightsFL75 = false;
@@ -24,9 +22,7 @@ function CombineExtensions:update(dt)
             self.beaconLightsFL50 = true;
             self:setBeaconLightsVisibility(true, false);
             self.beaconLightsOffDCB:call(2000);
-            if self.isEntered then
-                playSample(self.signalSample, 1, 1.2, 0);
-            end
+            CombineExtensions.playSample(self);
         elseif fillLevel <= (0.50 * capacity) and self.beaconLightsFL50 then
             self.beaconLightsFL50 = false;
         end
@@ -34,9 +30,7 @@ function CombineExtensions:update(dt)
             self.beaconLightsFL25 = true;
             self:setBeaconLightsVisibility(true, false);
             self.beaconLightsOffDCB:call(1000);
-            if self.isEntered then
-                playSample(self.signalSample, 1, 1.2, 0);
-            end
+            CombineExtensions.playSample(self);
         elseif fillLevel <= (0.25 * capacity) and self.beaconLightsFL25 then
             self.beaconLightsFL25 = false;
         end
@@ -44,17 +38,11 @@ function CombineExtensions:update(dt)
 end
 
 function CombineExtensions:postLoad(savegame)
-    local fileName = Utils.getFilename("sounds/warningSound.wav", CombineSignals.dir);
-    self.signalSample = createSample(fileName);
-    loadSample(self.signalSample, fileName, false);
-    local c = 0;
-    local s = 0;
+    CombineExtensions.loadSample();
+    self.beaconLightsOffDCB = DelayedCallBack:new(function(self)self:setBeaconLightsVisibility(false, false); end, self);
     for k, _ in pairs(self.beaconLights) do
         self.beaconLights[k].speed = self.beaconLights[k].speed * (math.random(70, 75) / 100);
-        c = c + 1;
-        s = s + self.beaconLights[k].speed;
     end
-    self.beaconLightsSpeed = s / c;
     local fillLevel = self:getUnitFillLevel(self.overloading.fillUnitIndex);
     local capacity = self:getUnitCapacity(self.overloading.fillUnitIndex);
     if fillLevel >= (0.25 * capacity) and not self.beaconLightsFL25 then
@@ -63,11 +51,28 @@ function CombineExtensions:postLoad(savegame)
     if fillLevel >= (0.50 * capacity) and not self.beaconLightsFL50 then
         self.beaconLightsFL50 = true;
     end
-    self.beaconLightsOffDCB = DelayedCallBack:new(function(self)self:setBeaconLightsVisibility(false, false); end, self);
 end
 
 function CombineExtensions:delete()
-    if self.signalSample ~= nil then
-        delete(self.signalSample);
+    if CombineExtensions.signalSample ~= nil then
+        delete(CombineExtensions.signalSample);
+    end
+end
+
+function CombineExtensions:playSample()
+    if self.isEntered then
+        local v = 0.8;
+        if self:getIsIndoorCameraActive() then
+            v = 1.4;
+        end
+        playSample(CombineExtensions.signalSample, 1, v, 0);
+    end
+end
+
+function CombineExtensions.loadSample()
+    if CombineExtensions.signalSample == nil then
+        local fileName = Utils.getFilename("sounds/warningSound.wav", CombineSignals.dir);
+        CombineExtensions.signalSample = createSample(fileName);
+        loadSample(CombineExtensions.signalSample, fileName, false);
     end
 end
